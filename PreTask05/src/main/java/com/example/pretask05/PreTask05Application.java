0afbafa421b6ff1fcf294efb7c5eba4ebed3e309
Tigris <kyrae604@naver.com>
2024-07-13 14:03:20 +0900

    ADD: Ch05 clone coding

---
+++ PreTask05/src/main/java/com/example/pretask05/PreTask05Application.java
@@ -0,0 +1,13 @@
+package com.example.pretask05;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PreTask05Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PreTask05Application.class, args);
+    }
+
+}
